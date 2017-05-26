56573096576956ae95f24006513e19f8eb57e67e
Luciano Camargo Cruz <luciano@lccruz.net>
2017-05-25 22:28:20 -0300

    Criado models, Noticia e Tags

---
+++ noticias/models.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+from django.db import models
+from tags.models import Tag
+
+
+class Noticia(models.Model):
+    tags = models.ManyToManyField(Tag)
+    title = models.CharField(
+        max_length=200,
+    )
+    url = models.URLField()
+    thumbnail = models.URLField(
+        blank=True,
+        null=True,
+    )
+    created_utc = models.DateTimeField()
