ea3f980b281c5a1fc02bb81c8ae3fdcd86af5ae1
Luciano Camargo Cruz <luciano@lccruz.net>
2017-05-27 11:43:00 -0300

    Adicionado tags na home

--- noticias/views.py
+++ noticias/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from noticias.models import Noticia
+from tags.models import Tag
 
 
 class HomePageView(TemplateView):
@@ -12,4 +13,5 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['noticias'] = Noticia.objects.all()[:15]
+        context['tags'] = Tag.objects.all()[:30]
         return context
